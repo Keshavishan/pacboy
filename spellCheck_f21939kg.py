6d507a5b6319eb28a41d85d8aaa5b0e73ffc8460
Keshav Gupta <keshav.gupta@student.manchester.ac.uk>
2021-11-08 20:17:14 +0000

    completed spellchecker

--- spellCheck_f21939kg.py
+++ spellCheck_f21939kg.py
@@ -2,37 +2,40 @@ import argparse
 from enum import Flag
 import re
 
-def getScore(score):
-    if score == "t":
-        return 5
-    elif score == "c":
-        return 2
+parser = argparse.ArgumentParser()
+parser.add_argument('dictionary', type=str, help='Path of dictionary file')
+parser.add_argument('input_file_path', type=str, help='Path of input file')
+parser.add_argument('output_file_path', type=str,
+                    help='Path of output file')
+
+args = parser.parse_args()
+
+inputFile = open(args.__dict__['input_file_path'], "r")
+sentences = inputFile.readlines()
+sentence = "".join(sentences)
+
+
+nos = re.findall("\d", sentence)
+punc = re.findall("[^a-zA-Z\d\s]", sentence)
+upper = re.findall("[A-Z]", sentence)
+
+sentence = re.sub("[^a-zA-Z\s]", "", sentence)
+sentence = sentence.lower()
+sentence = ' '.join(sentence.split())
+
+dictionary = open(args.__dict__['dictionary'])
+allWords = dictionary.readlines()
+words = sentence.split(" ")
+
+# print(words)
+
+correct, incorrect = 0, 0
+for word in words:
+    if (word + "\n" or word) in allWords:
+        correct = correct + 1
     else:
-        return 3
-
-
-if __name__ == '__main__':
-    word = "123"
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dictionary', type=str, help='Path of dictionary file')
-    parser.add_argument('input_file_path', type=str, help='Path of input file')
-    parser.add_argument('output_file_path', type=str, help='Path of output file')
-
-    args = parser.parse_args()
-
-    inputFile = open(args.__dict__['input_file_path'], "r")
-    sentences = inputFile.readlines()
-    sentence = "".join(sentences)
-    numbers = 0
-    punc = 0
-    uppercase = 0
-
-    nos = re.findall("\d", sentence)
-    punc = re.findall("[^a-zA-Z\d\s]", sentence)
-    upper = []
-
-    # sentence = re.sub("\d", "", sentence)
-    # out = open(args.__dict__['output_file_path'], "a")
-    # out.write(f'{str(T1)}:{str(T2)}')
-    # out.close()
+        incorrect = incorrect + 1
+out = open(args.__dict__['output_file_path'], "a")
+out.write(
+    f'Formatting ###################\nNumber of upper case words transformed: {str(len(upper))}\nNumber of punctuation’s removed: {str(len(punc))}\nNumber of numbers removed: {str(len(nos))}\nSpellchecking ###################\nNumber of words in file: {str(len(words))}\nNumber of correct words in file: {str(correct)}\nNumber of incorrect words in file: {str(incorrect)}')
+out.close()
