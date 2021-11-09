4358d973c716d6707f515afa3dd759a6984d397e
Keshav Gupta <keshav.gupta@student.manchester.ac.uk>
2021-11-09 14:40:40 +0000

    now looping through directory of inputs

--- rugby_f21939kg.py
+++ rugby_f21939kg.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 
@@ -9,16 +10,8 @@ def getScore(score):
     else:
         return 3
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_file_path', type=str, help='Path of input file')
-    parser.add_argument('output_file_path', type=str,
-                        help='Path of output file')
-
-    args = parser.parse_args()
-
-    inputFile = open(args.__dict__['input_file_path'], "r")
+def rugby(input_name, output_name):
+    inputFile = open(input_name, "r")
     score = inputFile.readline()
     n = 3
     scores = [score[i:i+n] for i in range(0, len(score), n)]
@@ -42,6 +35,34 @@ if __name__ == '__main__':
     else:
         print("T1 drew with T2")
 
-    out = open(args.__dict__['output_file_path'], "a")
+    out = open(output_name, "a")
     out.write(f'{str(T1)}:{str(T2)}')
     out.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_folder', type=str, help='Path of input folder')
+    parser.add_argument('output_folder', type=str,
+                        help='Path of output folder')
+
+    args = parser.parse_args()
+
+    inputPath = args.__dict__['input_folder']
+    outPath = args.__dict__['output_folder']
+
+    input_files = []
+
+    for entry in os.listdir(inputPath):
+        if os.path.isfile(os.path.join(inputPath, entry)):
+            input_files.append(os.path.join(inputPath, entry))
+
+    for file in input_files:
+        basename = os.path.basename(file)
+        print(basename)
+        f = os.path.splitext(basename)
+        
+        rugby(file, os.path.join(outPath, f[0] + "_f21939kg" + f[1]))
+
+
+   
