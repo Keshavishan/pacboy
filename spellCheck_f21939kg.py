f46a733a0d3556d1de252bf23ab5ff78186b2720
Keshav Gupta <keshav.gupta@student.manchester.ac.uk>
2021-11-09 14:54:22 +0000

    now reading inputs from directory

--- spellCheck_f21939kg.py
+++ spellCheck_f21939kg.py
@@ -1,41 +1,63 @@
+import os
 import argparse
 from enum import Flag
 import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument('dictionary', type=str, help='Path of dictionary file')
-parser.add_argument('input_file_path', type=str, help='Path of input file')
-parser.add_argument('output_file_path', type=str,
-                    help='Path of output file')
-
-args = parser.parse_args()
-
-inputFile = open(args.__dict__['input_file_path'], "r")
-sentences = inputFile.readlines()
-sentence = "".join(sentences)
+def spellchecker(dictionary, input_name, output_name):
+    inputFile = open(input_name, "r")
+    sentences = inputFile.readlines()
+    sentence = "".join(sentences)
 
 
-nos = re.findall("\d", sentence)
-punc = re.findall("[^a-zA-Z\d\s]", sentence)
-upper = re.findall("[A-Z]", sentence)
+    nos = re.findall("\d", sentence)
+    punc = re.findall("[^a-zA-Z\d\s]", sentence)
+    upper = re.findall("[A-Z]", sentence)
 
-sentence = re.sub("[^a-zA-Z\s]", "", sentence)
-sentence = sentence.lower()
-sentence = ' '.join(sentence.split())
+    sentence = re.sub("[^a-zA-Z\s]", "", sentence)
+    sentence = sentence.lower()
+    sentence = ' '.join(sentence.split())
 
-dictionary = open(args.__dict__['dictionary'])
-allWords = dictionary.readlines()
-words = sentence.split(" ")
+    dictionary = open(dictionary)
+    allWords = dictionary.readlines()
+    words = sentence.split(" ")
 
-# print(words)
+    correct, incorrect = 0, 0
+    for word in words:
+        if (word + "\n" or word) in allWords:
+            correct = correct + 1
+        else:
+            incorrect = incorrect + 1
+        
+    out = open(output_name, "a")
+    out.write(
+        f'Formatting ###################\nNumber of upper case words transformed: {str(len(upper))}\nNumber of punctuation’s removed: {str(len(punc))}\nNumber of numbers removed: {str(len(nos))}\nSpellchecking ###################\nNumber of words in file: {str(len(words))}\nNumber of correct words in file: {str(correct)}\nNumber of incorrect words in file: {str(incorrect)}')
+    out.close()
 
-correct, incorrect = 0, 0
-for word in words:
-    if (word + "\n" or word) in allWords:
-        correct = correct + 1
-    else:
-        incorrect = incorrect + 1
-out = open(args.__dict__['output_file_path'], "a")
-out.write(
-    f'Formatting ###################\nNumber of upper case words transformed: {str(len(upper))}\nNumber of punctuation’s removed: {str(len(punc))}\nNumber of numbers removed: {str(len(nos))}\nSpellchecking ###################\nNumber of words in file: {str(len(words))}\nNumber of correct words in file: {str(correct)}\nNumber of incorrect words in file: {str(incorrect)}')
-out.close()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dictionary', type=str, help='Path of dictionary folder')
+    parser.add_argument('input_folder', type=str, help='Path of input folder')
+    parser.add_argument('output_folder', type=str,
+                        help='Path of output folder')
+
+    args = parser.parse_args()
+
+    dictionary = args.__dict__['dictionary']
+    inputPath = args.__dict__['input_folder']
+    outPath = args.__dict__['output_folder']
+
+    input_files = []
+
+    for entry in os.listdir(inputPath):
+        if os.path.isfile(os.path.join(inputPath, entry)):
+            input_files.append(os.path.join(inputPath, entry))
+
+    if not os.path.exists(outPath):
+            os.makedirs(outPath)
+            
+    for file in input_files:
+        basename = os.path.basename(file)
+        f = os.path.splitext(basename)
+        
+        spellchecker(dictionary, file, os.path.join(outPath, f[0] + "_f21939kg" + f[1]))
+
