79a56fd34a6cf69ee49696c308cace7e544138e0
Keshav Gupta <keshav.gupta@student.manchester.ac.uk>
2021-11-14 21:32:38 +0000

    created function to read all images into a dictionary to then be used throughout the program began working on home screen

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from home import Home
+
+class Window():
+    def __init__(self, root: tk.Tk):
+        self.root = root
+
+        self.root.resizable(width=False, height=False)
+        self.root.title('Pacman')
+
+        Home(self.root)
+
+    def run(self) -> None:
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('1600x900')
+    pacman = Window(root)
+    pacman.run()
