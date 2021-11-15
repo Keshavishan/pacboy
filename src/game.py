4659f8ba73416b4ae90af81142e079cac96883f8
Keshav Gupta <keshav.gupta@student.manchester.ac.uk>
2021-11-15 21:57:45 +0000

    fixed pause game

--- src/game.py
+++ src/game.py
@@ -31,11 +31,9 @@ class Game():
 
         self.game = Maze(graphics)
         self.game.new_level()
-
-    def key_bindings(self, enabled: bool):
-        if enabled:
-            self.root.bind('<Escape>', self.pause_game)
-            self.root.bind('<space>', self.pause_game)
+    
+    def pause_game(self, event):
+        self.pause = not self.pause
     
     def draw_maze(self) -> None:
         height = self.height / self.game.m_height
@@ -47,13 +45,13 @@ class Game():
             elif type(obj) == Pacman or type(obj) == Dot or type(obj) == Enemy:
                 self.current.create_image( obj.x * width + (width / 2), obj.y * height + (height / 2), image = obj.avatar)
     
-    def refresh_board(self):
+    def refresh_maze(self):
         self.current.delete(tk.ALL)
         self.draw_maze()
     
     def countdown(self):
         def number(image):
-            self.refresh_board()
+            self.refresh_maze()
             self.current.create_image(self.width / 2, self.height / 2,
                                                   image = self.graphics.get(image) )
         
@@ -61,8 +59,6 @@ class Game():
         self.root.after(700, lambda: number('two'))
         self.root.after(1300, lambda: number('one'))
 
-    def pause_game(self, event):
-        self.pause = not self.pause
 
     def check_pause(self) -> None:
         if self.pause:
@@ -73,27 +69,25 @@ class Game():
     def exit(self):
         self.current.destroy()
         self.backFn()
-
-    def paused_screen(self):
-        self.current.create_image(self.width / 2, self.height / 2, image = self.graphics.get('paused') )
-        button1 = tk.Button(self.current, text = "Quit", image=self.graphics.get("back"), command=self.exit, anchor = tk.W)
-        button1.configure(width = 10, activebackground = "#33B5E5", relief = tk.FLAT)
-        self.current.create_window(10, 10, anchor=tk.NW, window=button1)
-
+        
     def update(self):
         if self.pause:
-            self.paused_screen()
+            self.current.create_image(self.width / 2, self.height / 2, image = self.graphics.get('paused') )
             self.check_pause()
 
         else:
-            pass
-            # self.game.update_directions()
+            self.game.update_directions()
             # self.game.update_board()
-            # self._check_for_completion()
+            self.root.after(125, self.update)
 
-            # if not self.board.game_over:
-            #     self._adjust_board()
+            if not self.game.game_over:
+                self.refresh_maze()
+    
+    def key_bindings(self, enabled: bool):
+        if enabled:
+            self.root.bind('<Escape>', self.pause_game)
+            self.root.bind('<space>', self.pause_game)
 
     def run(self):
         self.countdown()
-        self.root.after(500, self.update)
+        self.root.after(2000, self.update)
